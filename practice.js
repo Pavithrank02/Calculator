7beb65c0560e993404d1daaded20cb59db4af434
Pavithran <pavithrank02@gmail.com>
2022-07-28 08:26:21 +0530

    practice

---
+++ practice.js
@@ -0,0 +1,33 @@
+
+// const age1 = function (birthYear) {
+//     const age = 2022 - birthYear;
+//     const retirement = 60 - age;
+
+//     if(retirement>0) {
+//         return retirement;
+//         console.log()
+//     } else{
+//         return 'retired';
+//     }
+    
+// }
+// console.log(age1(1997));
+// console.log(age1(1960));
+
+// const calcTip = function (bill) {
+//     return bill>50 && bill<300 ? 
+//         bill * (15/100) : bill * (20/100);   
+// }
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(bills, tips);
+
+const  info = {
+    firstName: 'Pavi',
+    lastName: 'K',
+    age: 2022 -1997,
+    job: 'searching'
+};
+console.log(info.age);
+
+
